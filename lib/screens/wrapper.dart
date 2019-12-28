955a7af54d8b42cbc4147695931f1ccb64bfa760
ravshan <ravshanjavacoder@gmail.com>
2019-12-28 16:49:22 +0500

    anonymusly sign in

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,8 +1,6 @@
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 
-import 'home/home.dart';
-
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
---
+++ lib/services/auth.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anonymusly
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email & password
+
+  // register with email & password
+
+  // sign out
+}
